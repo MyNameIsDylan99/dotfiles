ba6162c6356465641ea0160a2790ae7794182755
Dylan <dylan.frosini@outlook.com>
2025-01-21 10:39:26 +0100

    Changes to dotfiles

--- nvim/.config/nvim/lua/dylan/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/dylan/plugins/colorscheme.lua
@@ -120,7 +120,17 @@ return {
 		name = "moonfly",
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme moonfly")
+			--vim.cmd("colorscheme moonfly")
+		end,
+	},
+	{
+		"tiagovla/tokyodark.nvim",
+		opts = {
+			-- custom options here
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts) -- calling setup is optional
+			vim.cmd([[colorscheme tokyodark]])
 		end,
 	},
 }
