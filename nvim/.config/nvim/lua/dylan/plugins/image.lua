ae0e4cb4ff0bdcfdc1cb0a82d61a1842fcdf6476
Dylan <dylan.frosini@outlook.com>
2025-03-27 11:32:11 +0100

    Updated configs

--- nvim/.config/nvim/lua/dylan/plugins/image.lua
+++ nvim/.config/nvim/lua/dylan/plugins/image.lua
@@ -6,7 +6,6 @@ return {
 		require("image").setup({
 			backend = "kitty",
 			processor = "magick_rock", -- or "magick_cli"
-			--processor = "magick_rock", -- or "magick_cli"
 			integrations = {
 				markdown = {
 					enabled = true,
