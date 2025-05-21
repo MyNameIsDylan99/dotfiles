51a947691bb62cad0928bcfd928f0d2781b5d555
MyNameIsDylan99 <=>
2025-05-21 22:25:13 +0200

    Changes to doom config

--- doom/config.el
+++ doom/config.el
@@ -1,55 +1,17 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; -*- lexical-binding: t; -*-
 (setq doom-theme 'doom-one)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 (setq-default truncate-lines t) ; in allen Modi keine Zeilen umbrechen
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-
 (after! org
-(setq org-directory "~/notes/")
-(setq org-agenda-files
-      '("~/notes/"
-        "~/notes/Life/GTD/Projects/"
-        "~/pCloudDrive/Portfolio/"
-        "~/notes/daily/"))
-(setq org-log-done 'time))
+  (setq org-directory "~/notes/")
+  (setq org-agenda-files
+        '("~/notes/"
+          "~/notes/Life/GTD/Projects/"
+          "~/pCloudDrive/Portfolio/"
+          "~/notes/daily/"))
+  (setq org-log-done 'time))
 
 ;; Schriftgrößen: skaliert Text sauber proportional zur UI
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 20))
@@ -67,9 +29,9 @@
 
 (after! lsp-mode
   (setq
-        lsp-enable-snippet nil     ;; optional
-        lsp-enable-symbol-highlighting t
-        lsp-signature-auto-activate t))
+   lsp-enable-snippet nil     ;; optional
+   lsp-enable-symbol-highlighting t
+   lsp-signature-auto-activate t))
 
 (add-hook 'csharp-mode-hook #'lsp!)
 
@@ -128,28 +90,48 @@
 (set-face-attribute 'org-level-3 nil :height 1.2 :weight 'bold)
 (set-face-attribute 'org-level-4 nil :height 1.1 :weight 'bold)
 
-;;(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
-;;(require 'mu4e)
-;;(require 'smtpmail)
-;;(setq user-mail-address "fresshnes@gmail.com"
-;;      user-full-name "Dylan Frosini"
-;;      mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbysyncrc -a"
-;;      mu4e-update-interval 300
-;;      message-signature
-;;      (concat
-;;       "Dylan Frosini\n")
-;;      message-send-mail-function 'smtpmail-send-it
-;;      starttls-use-gnuls t
-;;      smtpmail-starttls-credentials '(("smtp.land1.com" 587 nil nil))
-;;      smtpmail-auth-credentials '(("smtp.land1.com" 587 "fresshnes@gmail.com" nil))
-;;      smtpmail-default-smtp-server "smtp.land1.com"
-;;      smtpmail-smtp-service 587
-;;      mu4e-sent-folder "/Sent"
-;;      mu4e-drafts-folder "/Drafts"
-;;      mu4e-trash-folder "/Trash"
-;;      mu4e-refile-folder "/All Mail"
-;;      mu4e-maildir-shortcuts
-;;      '(("/gmail/INBOX"     . ?i)
-;;        ("/gmail/Sent"      . ?s)
-;;        ("/gmail/All Mail"  . ?a)
-;;        ("/gmail/Trash"     . ?t)))
+(use-package! treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+(use-package! astro-ts-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.astro\\'" . astro-ts-mode))
+  (when (modulep! +lsp)
+    (add-hook 'astro-ts-mode-hook (lambda () (lsp!)) 'append))
+  ;;(add-hook 'astro-ts-mode-hook #'lsp 'append)
+  ;;(add-hook 'astro-ts-mode-hook #'lsp! 'append))
+
+  :config
+  (let ((astro-recipe (make-treesit-auto-recipe
+                       :lang 'astro
+                       :ts-mode 'astro-ts-mode
+                       :url "https://github.com/virchau13/tree-sitter-astro"
+                       :revision "master"
+                       :source-dir "src")))
+
+    (add-to-list 'treesit-auto-recipe-list astro-recipe)))
+(set-formatter! 'prettier-astro
+  '("npx" "prettier" "--parser=astro"
+    "--config" "/home/dylan/.config/prettier/.prettierrc.mjs"
+    (apheleia-formatters-indent "--use-tabs" "--tab-width" 'astro-ts-mode-indent-offset))
+  :modes '(astro-ts-mode))
+
+(setf (alist-get 'astro-mode apheleia-mode-alist)
+      'prettier-astro)
+
+(use-package! lsp-tailwindcss
+  :when (modulep! +lsp)
+  :init
+  (setq! lsp-tailwindcss-add-on-mode t)
+  :config
+  (add-to-list 'lsp-tailwindcss-major-modes 'astro-ts-mode))
+
+;; MDX Support
+(add-to-list 'auto-mode-alist '("\\.\\(mdx\\)$" . markdown-mode))
+(when (modulep! +lsp)
+  (add-hook 'markdown-mode-local-vars-hook #'lsp! 'append))
+
