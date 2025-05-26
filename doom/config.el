d23862c332f4867005e243c5537ca88a2300eb00
MyNameIsDylan99 <=>
2025-05-26 14:38:33 +0200

    Auto-Commit: 2025-05-26 14:38:33

--- doom/config.el
+++ doom/config.el
@@ -62,6 +62,7 @@
       :desc "Tomorrow" "m" #'org-roam-dailies-capture-tomorrow
       :desc "Find daily" "d" #'org-roam-dailies-goto-date)
 
+
 ;;Smooth scrolling
 ;;(pixel-scroll-precision-mode 1)
 ;; (defun my-smooth-scroll (lines)
