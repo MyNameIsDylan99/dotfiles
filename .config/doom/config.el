1483251c460c7c5889dd8d60af595c5e28be61ab
MyNameIsDylan99 <=>
2025-06-18 21:41:55 +0200

    Auto-Commit: 2025-06-18 21:41:55

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -163,7 +163,7 @@
 
   ;; Formatter mit csharpier definieren
   (setf (alist-get 'csharpier apheleia-formatters)
-        '("csharpier" "format" filepath :in-place t))
+        '("clang-format" "--asume-filename=" filepath))
 
   ;; C#-Mode dem Formatter zuordnen
   (setf (alist-get 'csharp-ts-mode apheleia-mode-alist)
