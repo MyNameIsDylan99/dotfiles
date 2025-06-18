7a14f2f6616a45861d6a8cfa9baa27206b4f4567
MyNameIsDylan99 <=>
2025-06-18 20:50:28 +0200

    Auto-Commit: 2025-06-18 20:50:28

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -163,7 +163,7 @@
 
   ;; Formatter mit csharpier definieren
   (setf (alist-get 'csharpier apheleia-formatters)
-        '("csharpier" "--write-stdout"))
+        '("csharpier" "pipe-files"))
 
   ;; C#-Mode dem Formatter zuordnen
   (setf (alist-get 'csharp-ts-mode apheleia-mode-alist)
